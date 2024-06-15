ffc9de462bd61b06eaf28524270636ddf678c22b
van-dt <dangvan31102001@gmail.com>
2024-06-16 00:54:01 +0700

    fix location router

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,15 @@
 
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
   const router = useRouter();
-  router.push("/homepage");
+
+  useEffect(() => {
+    router.push("/homepage");
+  }, [router]);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
