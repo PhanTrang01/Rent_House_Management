2b89d3040948e5997dfaf2d5bcbafa1c1d80172a
PhanTrang01 <phantrang706@gmail.com>
2024-07-11 00:07:04 +0700

    aas

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,17 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export { prisma };
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ["query", "info", "warn", "error"],
+    // Optional: configure your pool size here
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL,
+      },
+    },
+  });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
